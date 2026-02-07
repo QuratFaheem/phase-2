a21f63fb450c27b3b5b0bd6f10379588cf5d7b41
QuratFaheem <mrs.faheem16@gmail.com>
2026-02-07 18:46:47 +0500

    done

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
+  // Enable experimental app dir (not needed in newer versions of Next.js)
+  // The app directory is now stable and enabled by default
 };
 
 module.exports = nextConfig;
